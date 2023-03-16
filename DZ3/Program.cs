e3bbc6f243aedb56b620ab6a129dbee18e9b5e13
Anastasia <anastasiastarodub@gmail.com>
2023-03-16 10:13:49 +0300

    решение задачи 3

---
+++ DZ3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Программа, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Таблица кубов чисел от 1 до " + N + ": ");
+
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine(i + " в кубе равно " + (i*i*i));
+}
+
